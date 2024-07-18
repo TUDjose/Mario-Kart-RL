34e7b5e2192b5e47c572f5e7355ffc3b53a7b909
TUDjose <J.B.PintodeMouraLeitedaCunha@student.tudelft.nl>
2024-07-18 20:30:46 +0100

    Readme and analytics

--- MKRL/analysis.py
+++ MKRL/analysis.py
@@ -4,53 +4,44 @@ import matplotlib.pyplot as plt
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 
-# Read Analytics file
-# file_path = r'Unity/analytics_20240711_145126.txt'
-# file_path = r'Unity/analytics_20240717_225726.txt'
-file_path = r'Unity/analytics_20240718_163200.txt'
-df = pd.read_csv(file_path, sep=';', header=None)
-df.columns = ['ID', 'FinishedLap', 'EpisodeLength', 'Rewards', 'Lesson', 'Step']
-df['ID'] = df['ID'].apply(lambda x: int(x[4:]))
-df['FinishedLap'] = df['FinishedLap'].apply(lambda x: bool(x))
-df['EpisodeLength'] = df['EpisodeLength'].apply(lambda x: int(x))
-df['Rewards'] = df['Rewards'].apply(lambda x: float(x.replace(',', '.')))
-df['Lesson'] = df['Lesson'].apply(lambda x: int(x))
-df['Step'] = df['Step'].apply(lambda x: int(x))
+def read_analytics():
+      for file_path in [r'Unity/analytics_20240718_163200.txt', r'Unity/analytics_20240718_193746.txt']:
+            df = pd.read_csv(file_path, sep=';', header=None)
+            df.columns = ['ID', 'FinishedLap', 'EpisodeLength', 'Rewards', 'Lesson', 'Step']
+            # df['ID'] = df['ID'].apply(lambda x: int(x[4:]))
+            df['FinishedLap'] = df['FinishedLap'].apply(lambda x: bool(x))
+            df['EpisodeLength'] = df['EpisodeLength'].apply(lambda x: int(x))
+            df['Rewards'] = df['Rewards'].apply(lambda x: float(x.replace(',', '.')))
+            df['Lesson'] = df['Lesson'].apply(lambda x: int(x))
+            df['Step'] = df['Step'].apply(lambda x: int(x))
 
 
-# Get some statistics
-fastest_lap = df[df['FinishedLap'] == True].sort_values(by='EpisodeLength').iloc[0]['EpisodeLength'] * 0.02
-total_step_count = df['EpisodeLength'].sum()
-num_episodes = len(df)
-percent_finished = len(df[df['FinishedLap'] == True]) / num_episodes
-better_than_human = len(df[(df['FinishedLap'] == True) & (df['EpisodeLength'] < 1870)]) / len(df[(df['FinishedLap'] == True)])
+            # Get some statistics
+            fastest_lap = df[df['FinishedLap'] == True].sort_values(by='EpisodeLength').iloc[0]['EpisodeLength'] * 0.02
+            total_step_count = df['EpisodeLength'].sum()
+            num_episodes = len(df)
+            percent_finished = len(df[df['FinishedLap'] == True]) / num_episodes
+            better_than_human = len(df[(df['FinishedLap'] == True) & (df['EpisodeLength'] < 1870)]) / len(df[(df['FinishedLap'] == True)])
 
-print(f'Fastest Lap: {fastest_lap} seconds',
-      f'Total Step Count: {total_step_count}',
-      f'Number of Episodes: {num_episodes}',
-      f'Percent Finished: {percent_finished * 100:.1f}%',
-      f'Better than human laps: {better_than_human * 100:.1f}%',
-      sep='\n')
+            print(f'Fastest Lap: {fastest_lap} seconds',
+                  f'Total Step Count: {total_step_count}',
+                  f'Number of Episodes: {num_episodes}',
+                  f'Percent Finished: {percent_finished * 100:.1f}%',
+                  f'Better than human laps: {better_than_human * 100:.1f}%',
+                  '\n',
+                  sep='\n')
 
 
-# Get Tensorboard csv files
-# cumulative_rewards = pd.read_csv(r'Unity/cumulative_reward.csv', header=0).iloc[:, 1:]
-# plt.plot(cumulative_rewards.iloc[:, 0], cumulative_rewards.iloc[:, 1])
-# plt.vlines(x=[5e5, 2e6, 5e6], ymin=-5, ymax=17, colors='r', linestyles='dashed', lw=1)
-# plt.xlabel('Step')
-# plt.ylabel('Cumulative Reward')
-# plt.grid()
-# plt.tight_layout()
-# plt.savefig('cumulative_rewards.png', dpi=300)
-# plt.show()
-
-# episode_lengths = pd.read_csv(r'Analytics/episode_length.csv', header=0).iloc[:, 1:]
-# plt.plot(episode_lengths.iloc[:, 0], episode_lengths.iloc[:, 1])
-# plt.vlines(x=[5e5, 2e6, 5e6], ymin=0, ymax=2100, colors='g', linestyles='dashed', lw=1)
-# plt.xlabel('Step')
-# plt.ylabel('Episode Length [steps]')
-# plt.grid()
-# plt.show()
+def cumulative_rewards():
+      cumulative_rewards = pd.read_csv(r'Unity/cumulative_reward.csv', header=0).iloc[:, 1:]
+      plt.plot(cumulative_rewards.iloc[:, 0], cumulative_rewards.iloc[:, 1])
+      plt.vlines(x=[5e5, 2e6, 5e6], ymin=-5, ymax=17, colors='r', linestyles='dashed', lw=1)
+      plt.xlabel('Step')
+      plt.ylabel('Cumulative Reward')
+      plt.grid()
+      plt.tight_layout()
+      plt.savefig('cumulative_rewards.png', dpi=300)
+      plt.show()
 
 
 def lr_sensitivity():
@@ -89,3 +80,11 @@ def gamma_sensitivity():
       plt.tight_layout()
       plt.savefig('gamma_sensitivity.png', dpi=300)
       plt.show()
+
+
+if __name__ == '__main__':
+        read_analytics()
+        cumulative_rewards()
+        lr_sensitivity()
+        epsilon_sensitivity()
+        gamma_sensitivity()
--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # Mario-Kart-RL
 AE4350 Bio-inspired intelligence and learning for aerospace applications assignment - using reinforcement learning (PPO) to create a Mario Kart like agent to complete a track
 
+## Code description
+The actual implementation of the PPO algorithm is provided by the Unity ML-Agents Toolkit, which contains a Python 
+API perform training of the environment created in Unity. To run new training, the CLI method can be used (please 
+refer to [Unity ML-Agents](https://github.com/Unity-Technologies/ml-agents) for further setup instructions):
+
+```bash
+mlagents-learn Assets/Configs/<config_file.yaml> --env=Builds --no_graphics
+```
+
+Nonetheless, the Agent and Environment need to be created in Unity. A build for this environment can be downloaded in 
+the Releases page, where the final RL model can be found and run to observe how it performs in the training and 
+testing tracks. Additionally, in this build, the tracks can also be manually attempted, to try to beat the agent's 
+lap time! 
+
+With regards to the code that makes up the agent and environment, these can be found in the `Assets/Scripts` 
+directory. Most important is the `KartAgent.cs` file, which contains the implementation of the reward function and 
+the agent's observation/state and actions. `TrainingManager.cs` also contains the implementation of the curriculum 
+learning methods (splitting training into different phases/lessons).
+
+## Results
+This video shows the agent going through the training track in real-time (not sped up). Such speeds and accuracy are 
+quite hard to match by the human controller.
 ![](https://github.com/TUDjose/Mario-Kart-RL/blob/main/Media/Training_track.gif)
